b8caddb313276428fca952081369911e10d7b298
Robbie Dornbush <dornbush@gwu.edu>
2018-06-24 23:40:59 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,6 @@
 
-describe('shout(string)', function() {
-  it('receives one argument and returns it in all caps', function() {
+describe('shout(string)', function shout(string) {
+  return string.toUppercase(), function() {
     expect(shout('hello')).toEqual('HELLO')
   })
 })
