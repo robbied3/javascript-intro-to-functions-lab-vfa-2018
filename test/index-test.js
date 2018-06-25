577d402af99bf1a866af299d0b569876f7a2767e
Robbie Dornbush <dornbush@gwu.edu>
2018-06-25 00:07:09 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -11,17 +11,9 @@ function logShout(string) {
   console.log(string.toUpperCase());
 }
 
-describe('logWhisper(string)', function() {
-  it('calls console.log() its one argument in all lowercase', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough()
-
-    logWhisper('HELLO')
-
-    expect(spy).toHaveBeenCalledWith('hello')
-
-    console.log.restore()
-  })
-})
+function logWhisper(string) {
+  console.log(string.toLowerCase()); 
+}
 
 describe('sayHiToGrandma(string)', function() {
   it('returns "I can\'t hear you!" if `string` is lowercase', function() {
